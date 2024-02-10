6e954b70d509a8dc9b75bd69dd5f65e0ec9854b8
Er-VinayKumar639 <123vinaychaurasiya@gmail.com>
2024-02-11 01:28:47 +0530

    Java-DSA practice code

---
+++ Java-DSA-Practice/FirstProgram.java
@@ -0,0 +1,43 @@
+//  class FirstProgram{
+//   public static void main(String a[]){
+//     System.out.println("Hello World!");
+//     System.out.println("Hello Coder!");
+
+//   }
+
+// }
+
+//  class FirstProgram2 {
+//   public static void main(String[] args) {
+//     System.out.println("Hello there! this is Vinay Kumar.");
+//   }
+//  }
+//   class FirstProgram4 {
+//   public static void main(String args[]){
+//       System.out.println("Hello Shashwat Tiwari 1");
+//       System.out.println("sona --> GOLD");
+//   }
+//   public static void main1(String args[]){
+//       System.out.println("Hello Shashwat Tiwari 4");
+//   }
+// }
+
+// class MyFirstClass {
+//   public static void main(String args[]){
+//       System.out.println("Hello Shashwat Tiwari 2");
+//   }
+// }
+
+// class MySecondClass {
+//   public static void main(String args[]){
+//       System.out.println("Hello Shashwat Tiwari 3");
+//   }
+// }
+
+
+
+
+
+
+
+
